339efe0863c19ed8ba063f376c34a3464043fc22
Otis Wright <chopfitzroy@gmail.com>
2018-12-20 12:30:13 +1300

    Pipe output to log instead of null

--- rofi-flatpak.sh
+++ rofi-flatpak.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+
+mkdir -p /tmp/flatpak-logs
+
 if [[ -z "$@" ]]; then
     find $HOME/.local/share/flatpak/exports/bin -maxdepth 1 -type l -printf '%f\n' 
 else
     # https://bbs.archlinux.org/viewtopic.php?id=229095
-    flatpak run $@ > /dev/null 2>&1 &
+    flatpak run $@ > "/tmp/flatpak-logs/$@.log" 2>&1 &
 fi
